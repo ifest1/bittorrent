d0b0a865ea4b66978462dc0f0fe73fe982b8f8f1
Iago Estrela <hiagoestrelas@gmail.com>
2021-05-24 22:33:09 -0300

    added file_pieces mount function

--- files/entity.py
+++ files/entity.py
@@ -8,8 +8,8 @@ class Files:
         self.file_size = file_size
         self.piece_length = piece_length
         self.set_pieces(pieces_hashes)
-        self.set_files(files)
         self.pieces_amount = len(self.pieces)
+        self.set_files(files)
 
     def __repr__(self):
         return self.name
@@ -35,41 +35,47 @@ class Files:
                 ))
     
     def set_files(self, files):
-        piece_index = 0
-        bytes_counter = 0
+        current_piece, current_file = 0, 0
+        while True:
+            if current_piece == self.pieces_amount-1: break
 
-        for _file in files:
-            file_pieces = []
-            file_length = _file["length"]
-            file_path = _file["path"]
+            file_path = files[current_file]["path"]
+            file_length = files[current_file]["length"]
+            piece = self.pieces[current_piece]
+        
+            pieces_amount, remaining_bytes = divmod(file_length + piece.get_byte_offset(), self.piece_length)
+
+            if pieces_amount:
+                piece_offset = piece.get_byte_offset()
+                file_pieces = self.pieces[current_piece:current_piece + pieces_amount]
+                piece.set_byte_offset(0)
+                self.files[tuple(file_path)] = (self.pieces[current_piece].get_byte_offset(), file_pieces)
+                current_piece += pieces_amount
+                piece = self.pieces[current_piece]
+                piece.set_byte_offset(remaining_bytes)
             
-            piece_bytes_fillment = file_length + self.pieces[piece_index].get_byte_offset()
-            pieces_amount, remaining_bytes = divmod(piece_bytes_fillment, self.piece_length)
-            
-            for index in range(pieces_amount): 
-                piece = self.pieces[index]
-                file_pieces.append(piece)
-                bytes_counter += self.piece_length
-                piece_index += 1
+            else:
+                piece_offset = piece.get_byte_offset()
+                
+                if (file_length + piece_offset) > self.piece_length:
+                    file_pieces = self.pieces[current_piece: current_piece + 1]
+                    self.files[tuple(file_path)] = (piece_offset, file_pieces)
+                    piece.set_byte_offset(0)
+                    current_piece += 1
+                    piece = self.pieces[current_piece]
+                    piece.set_byte_offset((file_length + piece_offset) % self.piece_length)
 
-            if remaining_bytes: 
-                current_piece_offset = self.pieces[piece_index].get_byte_offset()
-                piece_bytes_fillment = current_piece_offset + remaining_bytes
-                next_piece_offset = remaining_bytes
-
-                if piece_bytes_fillment > self.piece_length:
-                    next_piece_offset = piece_bytes_fillment % self.piece_length
-                    piece_bytes_fillment -= next_piece_offset
-                    piece = self.pieces[piece_index]
-                    piece.set_byte_offset(next_piece_offset)
-                    piece_index += 1
-
+                elif (file_length + piece_offset) == self.piece_length:
+                    file_piece = [piece]
+                    piece.set_byte_offset(0)
+                        
                 else:
-                    piece = self.pieces[piece_index]
-                    next_piece_offset += piece.get_byte_offset()
-                    piece.set_byte_offset(next_piece_offset)
-                    file_pieces.append(piece)
-                print(piece_index)
-                print(file_pieces)
+                    file_piece = [piece]
+                    piece.set_byte_offset(file_length + piece_offset)
 
-            self.files[tuple(file_path)] = file_pieces
+                    self.files[tuple(file_path)] = (piece_offset, file_piece)
+
+            current_file += 1
+           
+        for key, value in self.files.items():
+            print(key, value)
